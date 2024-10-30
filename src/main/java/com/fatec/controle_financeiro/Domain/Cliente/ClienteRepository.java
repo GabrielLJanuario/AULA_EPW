e52460286b05237688ba85fb98ed8ceceb909101
GabrielLJanuario <148555743+GabrielLJanuario@users.noreply.github.com>
2024-10-30 19:48:36 -0300

    Atualização de 30/09/2024

--- src/main/java/com/fatec/controle_financeiro/Domain/Cliente/ClienteRepository.java
+++ src/main/java/com/fatec/controle_financeiro/Domain/Cliente/ClienteRepository.java
@@ -2,6 +2,5 @@ package com.fatec.controle_financeiro.Domain.Cliente;
 
 import com.fatec.controle_financeiro.entities.ContasPagar;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface ClienteRepository extends JpaRepository<ContasPagar, Long> {
 }
